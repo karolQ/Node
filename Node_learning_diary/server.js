f685a253a86257cae91716036d0d2c8f5af81ac7
Karol <yingkunli512@gmail.com>
2018-04-12 17:34:01 -0400

    add 404 response

--- Node_learning_diary/server.js
+++ Node_learning_diary/server.js
@@ -1,13 +1,24 @@
 // create a basic server
 var http = require('http');
-var fs = require("fs");
+var fs = require('fs');
 
-
-function onRequest(request, response){
-    consol.log("A user made a request" + request.url);
-    response.writeHead(200, {"Context-Type": "text/plain"});
-    resposne.write("Here is some data.");
+// adding 404 response for unknow responses
+function send404Response(response){
+    response.writeHead(404,{"Context-Type": "text/plain"});
+    response.write("Error 404: Page not found!");
     response.end();
+}
+
+// Handle a user's request
+function onRequest(request, response){
+    if(request.method == 'GET' && request.url == '/'){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream("./index.html").pipe(response);
+    }
+    else{
+        send404Response(response);
+    }
+    // response.end();
 }
 
 http.createServer(onRequest).listen(3030);
