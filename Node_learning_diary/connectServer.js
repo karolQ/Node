50a0c64cd67afadfed3d7fcf05778e6e856360bc
Karol <yingkunli512@gmail.com>
2018-04-13 23:01:42 -0400

    more about connect module

--- Node_learning_diary/connectServer.js
+++ Node_learning_diary/connectServer.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var http = require('http');
 
 var app = connect();
-
+/* connect demo1
 function doFirst(request, response, next){
     console.log("Something");
     // if not calling next(); the doSecond function wouldn't run
@@ -16,6 +16,21 @@ function doSecond(request, response, next){
 
 app.use(doFirst);
 app.use(doSecond);
+*/
+
+// connect demo2
+function profile(request, response){
+    console.log("User requested profile");
+
+}
+
+function forum(request, response){
+    console.log("User requested forum");
+
+}
+
+app.use('/profile', profile);
+app.use('/forum', forum);
 
 http.createServer(app).listen(3030);
 console.log("Server is running");
